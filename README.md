15dd8d6830a563a864a254417af8ef257aa9f8ef
charles-liu <charles@vite.org>
2022-01-16 14:54:45 +0800

    add TeamLock contract and tests

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # Vite-Contract
 
-Solidity++ contracts in Vite block chain.
+Solidity++ contracts on Vite blockchain.
 
 ## LockToken Contract
 
 Token Lockup Contract of Vite Labs Team.
 
 20% of VITE coins are held by Vite Labs team and locked up for 2 years. These part of coins will be unlocked since 1st Jun 2020, from 1st Jun 2020 to 1st Dec 2020, 1.5m VITE will be unlocked every 30 days. The rest of coins will be unlocked on 1st Jan 2022.
+
+## TeamLock Contract
+### Team's Token Lock Contract V2.
+
+- Unlock 1.5M VITE every 30 days since Feb 1 2022.
+- Unlock all remaining tokens after Jan 1 2024.
+- In an emergency, permanently lock all remaining tokens and wait for a hard fork.
+
+### Test 
+```
+npm test
+```
 
 ## FullNodeStake Contract
 
---
+++ test/TeamLock/params.spec.ts
@@ -0,0 +1,54 @@
+import { describe } from "mocha";
+import { expect } from "chai";
+import * as compiler from "../../src/legacyCompiler";
+import * as vite from "../../src/vite";
+import config from "../vite.config.json";
+
+let provider: any;
+let deployer: any;
+
+
+describe('TeamLock Constructor Params', () => {
+  before(async function() {
+    provider = vite.localProvider();
+    deployer = vite.newAccount(config.networks.local.mnemonic, 0);
+  });
+
+  it('test contructor params', async () => {
+    // compile contract
+    const compiledContracts = await compiler.compile('TeamLock.solpp');
+    expect(compiledContracts).to.have.property('TeamLock');
+
+    // deploy contract
+    let teamLock = compiledContracts.TeamLock;
+    teamLock.setDeployer(deployer).setProvider(provider);
+
+    await teamLock.deploy({
+      params: [
+        deployer.address!,
+        deployer.address!,
+        '0',
+        '0',
+        '0',
+        '0'
+      ],
+      responseLatency: 1
+    });
+    expect(teamLock.address).to.be.a('string');
+
+    // params in production environment
+    let startTime = Math.round(new Date('Feb 01 2022').getTime() / 1000); // Feb 01 2022
+    let interval = 30 * 24 * 3600; // 30 days in seconds
+    let endTime = Math.round(new Date('Jan 01 2024').getTime() / 1000); // Jan 01 2024
+    let unlockAmount = '1500000000000000000000000';
+
+    // check params
+    expect(await teamLock.query('getNextTime', [deployer.address])).to.be.deep.equal([startTime.toString()]);
+    expect(await teamLock.query('getEndTime', [deployer.address])).to.be.deep.equal([endTime.toString()]);
+    expect(await teamLock.query('getInterval', [deployer.address])).to.be.deep.equal([interval.toString()]);
+    expect(await teamLock.query('getUnlockAmount', [deployer.address])).to.be.deep.equal([unlockAmount.toString()]);
+    expect(await teamLock.query('getBenificiary', [])).to.be.deep.equal([deployer.address]);
+    expect(await teamLock.query('isOwner', [deployer.address])).to.be.deep.equal(['1']);
+
+  });
+});
